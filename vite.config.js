5f95d5e7e28178b9c568fcef535384f13af0c2dd
Denys <levchenkoden02@gmail.com>
2024-08-25 14:20:04 +0300

    Fix routing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path"; // Додайте цей імпорт
 
 export default defineConfig({
   plugins: [vue()],
@@ -12,12 +12,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // Порт вашего backend сервера
+        target: "http://localhost:3000", // Порт вашого backend сервера
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
-
-
