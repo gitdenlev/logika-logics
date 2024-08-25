c44d8f4dfae4d908bd3aa8570a385506fd9d9cdd
Denys <levchenkoden02@gmail.com>
2024-08-25 14:06:19 +0300

    Fix routing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
@@ -14,3 +19,5 @@ export default defineConfig({
     },
   },
 });
+
+
