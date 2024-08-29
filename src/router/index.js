3ec54acd7a3d8cec5112722679915d3842a45e2a
Denys <levchenkoden02@gmail.com>
2024-08-29 15:36:29 +0300

    Fix routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHasHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
-import NotFound from '../views/NotFound.vue';
+// import NotFound from '../views/NotFound.vue';
 import Scratch from "../views/Scratch.vue";
 import Gamedesign from "../views/Gamedesign.vue";
 import Webdesign from '../views/Webdesign.vue';
@@ -9,45 +9,33 @@ import Frontend from "../views/Frontend.vue";
 
 
 
-const router = createRouter({
+export default createRouter({
   history: createWebHasHistory(),
-  routes: [
+ routes: [
     {
       path: "/",
-      name: "home",
       component: HomeView,
     },
     {
-      path: "/:pathMatch(.*)*",
-      name: "NotFound",
-      component: NotFound,
-    },
-    {
       path: "/scratch",
-      name: "Scratch",
       component: Scratch,
     },
     {
       path: "/gamedesign",
-      name: "Gamedesign",
       component: Gamedesign,
     },
     {
       path: "/webdesign",
-      name: "Webdesign",
       component: Webdesign,
     },
     {
       path: "/python",
-      name: "Python",
       component: Python,
     },
     {
       path: "/frontend",
-      name: "Frontend",
       component: Frontend,
     },
   ],
-});
+})
 
-export default router
